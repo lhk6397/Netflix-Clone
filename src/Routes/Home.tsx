350723543491818830ba0b333946aa5ce61f8eb3
maruhxn <a23686397@gmail.com>
2022-06-23 09:23:08 +0900

    Header 작업 1차
    Header Componets 추가 및 CSS 작업

---
+++ src/Routes/Home.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div style={{ backgroundColor: "#141414", height: "200vh" }}></div>;
+};
+
+export default Home;
--- src/index.tsx
+++ src/index.tsx
@@ -74,6 +74,7 @@ a {
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <RecoilRoot>
