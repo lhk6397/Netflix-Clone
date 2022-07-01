c34a65b04a44fb1cdd295a5b15d349645ce94ce3
maruhxn <a23686397@gmail.com>
2022-07-01 23:36:08 +0900

    Add Box Animation 2
    Add Info animation div

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -64,6 +64,19 @@ const Box = styled(motion.div)<{ bgImage: string }>`
   }
 `;
 
+const Info = styled(motion.div)`
+  padding: 20px;
+  background-color: ${(props) => props.theme.black.lighter};
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  h4 {
+    text-align: center;
+    font-size: 18px;
+  }
+`;
+
 const rowVariants = {
   hidden: {
     x: window.outerWidth + 5,
@@ -83,6 +96,17 @@ const boxVariants = {
   hover: {
     scale: 1.3,
     y: -50,
+    transition: {
+      delay: 0.5,
+      duration: 0.3,
+      type: "tween",
+    },
+  },
+};
+
+const infoVariants = {
+  hover: {
+    opacity: 1,
     transition: {
       delay: 0.5,
       duration: 0.3,
@@ -144,7 +168,11 @@ const Home = () => {
                       variants={boxVariants}
                       transition={{ type: "tween" }}
                       bgImage={makeImagePath(movie.backdrop_path, "w500")}
-                    ></Box>
+                    >
+                      <Info variants={infoVariants}>
+                        <h4>{movie.title}</h4>
+                      </Info>
+                    </Box>
                   ))}
               </Row>
             </AnimatePresence>
